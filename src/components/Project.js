e1c616de2dd8f693cca79f7505b2e1b3aa2936bf
dahyun <kkdh0130@gmail.com>
2025-02-14 16:59:35 +0900

    odify common.css

--- src/components/Project.js
+++ src/components/Project.js
@@ -144,7 +144,7 @@ function Project() {
       </div>
       <div className="project-content d-flex flex-column justify-content-center align-items-center text-center w-100">
         <motion.div
-          initial={{ opacity: 0, y: 50 }}
+          initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
         >
